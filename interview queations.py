49003ca49739ca41a0ad78b95a7741bd1184c90e
Anjiyadav <anjaneyulusanneboina@gmail.com>
2022-12-22 09:04:52 -0500

    add

---
+++ interview queations.py
@@ -0,0 +1,24 @@
+                                              Interview Questions
+
+
+1.What is List & Dictionary comprehension? Write an example for each.
+2.what is Lambda function? Give one example.
+3.*
+   **
+  ***
+  ****
+Print this pattern
+4.what is split() and join() function used for write an example
+5.what is class & object? Create a class and object
+6.what is constructor method ? can we replace the default parameter self with other ? create constructor
+7.what are the differences between List and Tuple?
+8.what is pickling &unpickling?
+9.what is the difference between range and xrange?
+10.write a program in which we take string and count the occurrence of each character in the given string .ex:(a:2,b:1)?
+11.what is Django rest api?
+12.what are the methods in rest api?
+13.what is the purpose of models &serializers?
+14.create one model?
+15.what is signalling in Django?
+16.what is map function in python?
+17.what is set?
